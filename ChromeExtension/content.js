2ed312a19e3f6d3fd3e61b281d16d40882f67912
Sam Randall <sam.randall@livetiles.nyc>
2018-09-08 18:22:03 +1000

    added notifications

--- ChromeExtension/content.js
+++ ChromeExtension/content.js
@@ -1,11 +1,34 @@
 var btn = document.createElement("DIV");
 btn.className = 'psa-button';
 
-var img =  document.createElement("IMG");
+var img = document.createElement("IMG");
 img.setAttribute("src", chrome.extension.getURL("geoffrey-house.png"));
 img.setAttribute("width", "50");
 img.setAttribute("height", "50");
 img.className = 'psa-button-image';
 btn.appendChild(img);
 
+var botContainer = document.createElement("DIV");
+botContainer.className = 'psa-bot-container';
+
 document.body.appendChild(btn);
+document.body.appendChild(botContainer);
+
+Notification.requestPermission(function (status) {
+    console.log('Notification permission status:', status);
+});
+
+btn.onclick = () => {
+    console.log('Notification Permission: ', Notification.permission);
+    if (Notification.permission === 'granted') {
+        var options = {
+            body: '... is cooked.',
+            icon: chrome.extension.getURL("geoffrey-house.png")
+        };
+        var notification = new Notification("We found something!", options);
+
+        notification.onclick = () => {
+            botContainer.style.display = 'block';
+        }
+    }
+};
